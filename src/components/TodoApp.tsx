0a85e28e9a1b624caf151cf8db671c98a523e5ed
jyllatupa <chelsea_30_01@hotmail.com>
2024-05-31 22:17:37 -0500

    se agrego el style flex en TodoApp.tsx

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -19,7 +19,7 @@ export const TodoApp = () => {
     return(
         <div>
             <h1>Lista de Tareas</h1>
-            <div className="divtarea">
+            <div className="flex">
                 <input
                     type="text"
                     value={nuevaTarea}
